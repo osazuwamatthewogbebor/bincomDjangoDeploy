fe77821a0c338e33514f6c5167e3eaf2a2baa8d9
osazuwamatthewogbebor <osazuwamatthewogbebor@gmail.com>
2025-07-19 11:56:25 +0100

    Prepare for Railway deployment

--- deploybincom/settings.py
+++ deploybincom/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)wx2*-_^j&$&k4-7_bh-6uc7=yy!ddo0#ay2a25cden1k4-(z3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -30,10 +30,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'electionApp'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -112,7 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+# For compressed static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ electionApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ElectionappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'electionApp'
